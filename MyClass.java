5c7940f532d6bc1713a51cd907ec89f6488adefd
cemergin77 <48433561+cemergin77@users.noreply.github.com>
2021-05-05 14:02:58 +0300

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,29 @@
+package com.cem.mypackage;
+
+import java.lang.System.*;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int i=1;i<=100;i++)
+        {
+        	if(i%15==0)
+        		System.out.println("FizzBuzz");
+        	else if(i%3==0)
+        	{
+        		System.out.println("Fizz");
+        	}
+        	else if(i%5==0)
+        	{
+        		System.out.println("Buzz");
+        	}
+        	else
+        	{
+        		System.out.println(i);
+        	}
+        		
+        }
+	}
+
+}
